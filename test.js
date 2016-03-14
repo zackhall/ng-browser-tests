a4e4f3b4aa93cbbf38be0e6462e2deba9348df67
Zack Hall <zackhall@outlook.com>
2016-03-14 09:49:41 -0700

    Initial commit

---
+++ test.js
@@ -0,0 +1,42 @@
+var ngDir = process.argv[2];
+var ngFiles = process.argv[3];
+
+var angularFiles = require(ngFiles); 
+var glob = require('glob');
+var _ = require('lodash');
+var path = require('path');
+var nunjucks = require('nunjucks');
+var fs = require('fs');
+
+var template = nunjucks.compile(fs.readFileSync('_template.html', 'utf8').toString());
+
+var config = {
+  modules: {
+    include: angularFiles.mergeFilesFor('karmaModules', 'angularSrcModules'),
+    exclude: []
+  },
+  jquery: {
+    include: angularFiles.mergeFilesFor('karmaJquery'),
+    exclude: angularFiles.mergeFilesFor('karmaJqueryExclude')
+  }, 
+  jqlite: {
+    include: angularFiles.mergeFilesFor('karma'),
+    exclude: angularFiles.mergeFilesFor('karmaExclude')
+  }
+}
+
+var includes = _.transform(config, function(result, value, key) {
+  var files = _.flatMap(value.include, function(pattern) {
+    return glob.sync(pattern, { cwd: ngDir, ignore: value.exclude });
+  });
+
+  result[key] = 
+    files.map(function(value) {
+      var href = path.join(ngDir, value);
+      return `<script href="${href}"></script>`;
+    }).join('\n');
+});
+
+_.forIn(includes, function(value, key) {
+  fs.writeFileSync(path.join(__dirname, 'out', key + '.html'), template.render({ scripts: value }), 'utf-8');
+});
