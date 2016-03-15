0c0f3477eb28711aa76354dd7124be5d57446713
Zack Hall <zackhall@outlook.com>
2016-03-15 13:49:53 -0700

    Point script at submodule.

--- test.js
+++ test.js
@@ -1,16 +1,19 @@
-var ngDir = process.argv[2];
-var ngFiles = process.argv[3];
+var config = {
+  ngDir: './ng',
+  ngFiles: './ng/angularFiles.js'
+};
 
-var angularFiles = require(ngFiles); 
+var angularFiles = require(config.ngFiles); 
 var glob = require('glob');
 var _ = require('lodash');
 var path = require('path');
 var nunjucks = require('nunjucks');
 var fs = require('fs');
+var mkdirp = require('mkdirp');
 
 var template = nunjucks.compile(fs.readFileSync('_template.html', 'utf8').toString());
 
-var config = {
+var tests = {
   modules: {
     include: angularFiles.mergeFilesFor('karmaModules', 'angularSrcModules'),
     exclude: []
@@ -23,23 +26,27 @@ var config = {
     include: angularFiles.mergeFilesFor('karma'),
     exclude: angularFiles.mergeFilesFor('karmaExclude')
   }
-}
+};
 
-var includes = _.transform(config, function(result, value, key) {
+var includes = _.transform(tests, function(result, value, key) {
   var files = _.flatMap(value.include, function(pattern) {
-    return glob.sync(pattern, { cwd: ngDir, ignore: value.exclude });
+    return glob.sync(pattern, { cwd: config.ngDir, ignore: value.exclude });
   });
 
   result[key] = 
     _(files)
       .uniq()
       .map(function(value) {
-        var src = path.join('..', ngDir, value);
+        var src = path.join('..', config.ngDir, value);
         return `<script src="${src}"></script>`;
       })
       .join('\n');
 });
 
-_.forIn(includes, function(value, key) {
-  fs.writeFileSync(path.join(__dirname, 'out', key + '.html'), template.render({ scripts: value }), 'utf-8');
+mkdirp(path.join(__dirname, 'out'), function(err) {
+  if (err) console.log(err);
+
+  _.forIn(includes, function(value, key) {
+    fs.writeFileSync(path.join(__dirname, 'out', key + '.html'), template.render({ scripts: value }), 'utf-8');
+  });
 });
